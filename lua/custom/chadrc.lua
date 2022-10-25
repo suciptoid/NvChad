e12c95cc8cf956131d833bc4cadb90bdece1eec8
Sucipto <sucipto@pm.me>
2022-10-25 16:30:48 +0700

    custom

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,7 @@
+local M = {}
+M.plugins = require "custom.plugins"
+M.ui = {
+  -- theme = "vscode_dark"
+}
+
+return M
---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,35 @@
+return {
+  ["folke/which-key.nvim"] = { disable = false },
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.null-ls"
+    end,
+  },
+  ["williamboman/mason.nvim"] = {
+    override_options = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "deno",
+        "emmet-ls",
+        "intelephense",
+        "phpcs",
+        "php-cs-fixer",
+
+        "prettierd",
+        "vetur-vls",
+      },
+    },
+  },
+}
