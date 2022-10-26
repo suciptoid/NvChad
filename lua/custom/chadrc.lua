d2d3ae25e019817cdd9119d369982903c2103f5a
Sucipto <sucipto@pm.me>
2022-10-26 11:59:21 +0700

    change update url

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,5 +1,13 @@
 local M = {}
 M.plugins = require "custom.plugins"
+
+M.options = {
+  nvChad = {
+    update_url = "https://github.com/suciptoid/NvChad",
+    update_branch = "custom",
+  },
+}
+
 M.ui = {
   -- theme = "vscode_dark"
 }
